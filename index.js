3a06e52ac231f1120388329dcead332465eac2e6
IntegerAlex <inquiry.akshatkotpalliwar@gmail.com>
2023-08-25 21:40:22 +0530

    timestamp
    freecode camp

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
 // index.js
-// where your node app starts
-
-// init project
 var express = require('express');
 var app = express();
 
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
+// enable CORS
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 }));
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -18,15 +14,35 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
-// your first API endpoint... 
+// your first API endpoint...
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  res.json({ greeting: 'hello API' });
 });
 
+app.get("/api/:date?", function (req, res) {
+  let inputDate = req.params.date;
+  
+  if (!inputDate) {
+    inputDate = new Date();
+  } else if (/\d{5,}/.test(inputDate)) {
+    inputDate = parseInt(inputDate);
+  } else {
+    inputDate = new Date(inputDate);
+  }
+  
+  const dateObject = new Date(inputDate);
+  
+  if (dateObject.toString() === "Invalid Date") {
+    res.json({ error: "Invalid Date" });
+  } else {
+    res.json({
+      unix: dateObject.getTime(),
+      utc: dateObject.toUTCString()
+    });
+  }
+});
 
-
-// listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+// listen for requests
+var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
